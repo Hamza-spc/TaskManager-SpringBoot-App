2fcd33b427f1d8e61859b14d8e90d541d5b8579a
Hamza <hamza.2simou@gmail.com>
2026-03-17 17:39:55 +0000

    add User entity

---
+++ src/main/java/com/hamza/taskmanager/entity/User.java
@@ -0,0 +1,44 @@
+package com.hamza.taskmanager.entity;
+
+import com.hamza.taskmanager.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private UserRole role = UserRole.USER;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+}
