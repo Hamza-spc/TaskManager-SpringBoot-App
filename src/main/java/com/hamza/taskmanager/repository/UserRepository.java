724f91cda33e7598c86b803a3d332351afbe44fa
Hamza <hamza.2simou@gmail.com>
2026-03-17 22:51:06 +0000

    add TaskRepository

--- src/main/java/com/hamza/taskmanager/repository/UserRepository.java
+++ src/main/java/com/hamza/taskmanager/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.hamza.taskmanager.repository;
 
+
 import com.hamza.taskmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository <User,Long> {
     Optional<User> findByEmail(String email);
     boolean existByEmail(String email);
 }
