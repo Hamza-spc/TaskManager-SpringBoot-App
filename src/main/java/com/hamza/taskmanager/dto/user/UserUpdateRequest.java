68fd5f71935046bdf302872c445f10b552057a31
Hamza <hamza.2simou@gmail.com>
2026-03-21 20:22:37 +0000

    add UserUpdateRequest DTO

--- src/main/java/com/hamza/taskmanager/dto/user/UserUpdateRequest.java
+++ src/main/java/com/hamza/taskmanager/dto/user/UserUpdateRequest.java
@@ -1,8 +1,20 @@
 package com.hamza.taskmanager.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserUpdateRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max = 20, message = "Name must not exceed 100 characters")
+    private String name;
 
+    @NotBlank(message = "Email is required")
+    @Size(max = 50, message = "Email must not exceed 50 characters")
+    private String email;
+
+    @NotBlank
+    @Size(min = 8,max = 100, message = "Password must have at least 8 characters")
+    private String password;
 }
