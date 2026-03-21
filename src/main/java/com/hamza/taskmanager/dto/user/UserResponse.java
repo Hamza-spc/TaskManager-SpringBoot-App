4e18a7bb34b40ef389772c290bbddac82bae8f9d
Hamza <hamza.2simou@gmail.com>
2026-03-21 20:39:45 +0000

    add UserResponse DTO

--- src/main/java/com/hamza/taskmanager/dto/user/UserResponse.java
+++ src/main/java/com/hamza/taskmanager/dto/user/UserResponse.java
@@ -1,4 +1,17 @@
 package com.hamza.taskmanager.dto.user;
 
+import com.hamza.taskmanager.enums.UserRole;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private UserRole role;
+    private LocalDateTime createdAt;
 }
